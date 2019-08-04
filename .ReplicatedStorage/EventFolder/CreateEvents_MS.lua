ee610ebc7b249ae2ad8b0388b14b914dba4c855c
EXYZED <dntJacques@gmail.com>
2019-08-04 10:30:28 -0400

    Created CreateEvents.lua

---
+++ .ReplicatedStorage/EventFolder/CreateEvents_MS.lua
@@ -0,0 +1,20 @@
+--[[
+
+File:           CreateEvents.lua (ModuleScript)
+Description:    Creates events.
+
+Pre-requisites: FE Compatability, Efficiency.
+
+Issuer, A:      XLNS_XYZ Danté#9120
+Privilege:      Y
+
+]]
+
+local Module = {}
+
+function EventName()
+
+end
+-- ...
+
+return Module
