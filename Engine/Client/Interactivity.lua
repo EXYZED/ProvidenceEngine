7d0463306c1cf814f194903347e86319a1c0647f
EXYZED <dntJacques@gmail.com>
2019-08-04 10:15:41 -0400

    Added sample files.

---
+++ Engine/Client/Interactivity.lua
@@ -0,0 +1,11 @@
+--[[
+
+File:           Interactivity.lua (Localscript)
+Description:    Variable handler, for efficiency.
+
+Pre-requisites: FE Compatability, Efficiency.
+
+Issuer, A:      XLNS_XYZ Danté#9120
+Privilege:      Y
+
+]]
---
+++ Engine/Server/CelestialControl.lua
@@ -0,0 +1,12 @@
+--[[
+
+File:           CelestialControl.lua (ServerScript)
+Description:    Should handle TimeOfDay, requests to change time through
+                a connected remote event.
+
+Pre-requisites: FE Compatability, Efficiency.
+
+Issuer, A:      XLNS_XYZ Danté#9120
+Privilege:      Y
+
+]]
