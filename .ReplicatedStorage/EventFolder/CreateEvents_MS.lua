e7fd9dd1e64e2ef16405059cd68abc54d7957621
EXYZED <dntJacques@gmail.com>
2019-08-04 10:42:52 -0400

    general edits

--- .ReplicatedStorage/EventFolder/CreateEvents_MS.lua
+++ .ReplicatedStorage/EventFolder/CreateEvents_MS.lua
@@ -10,11 +10,31 @@ Privilege:      Y
 
 ]]
 
+local EventDirectory = game:GetService'ReplicatedStorage':WaitForChild'EventFolder';
+---
 local Module = {}
 
-function EventName()
+function CreateEvents()
+  -- Event to deal with sprint
+  local SprintRemote = Instance.new("RemoteEvent");
+  SprintRemote.Parent = EventDirectory;
+  SprintRemote.OnServerEvent:Connect(function(player,info))
+      if tostring(info) == "EndSprint" then
+          player.Character.Humanoid.WalkSpeed = 16
+      end
+      if tostring(info) == "StartSprint" then
+          player.Character.Humanoid.WalkSpeed = sprintSpeed
+      end
+  end)
+  --
+  local GenericEvent = Instance.new("RemoteEvent")
+  GenericEvent.Parent = EventDirectory;
+  SprintRemote.OnServerEvent:Connect(function()
+    print'Hello World!'
+  end)
+  --
 
-end
--- ...
+
+end -- end createfunction.
 
 return Module
